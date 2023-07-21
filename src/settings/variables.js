913d5a333f33ff56acecf4da6343e6b4315c95bd
OlgaMunaieva <asyssync@gmail.com>
2023-07-21 08:16:42 +0300

    added all colors

--- src/settings/variables.js
+++ src/settings/variables.js
@@ -2,7 +2,11 @@ const colors = {
   accentHeaderButton: '#FFC107',
   background: '#FDF7F2',
   text: '#000000',
+  buttonsGr: 'linear-gradient(290.46deg, #419EF1 0%, #9BD0FF 107.89%)',
   buttonsHoverBg: '#54ADFF',
+  cardsButtonBg: '#CCE4FB',
+  accentAddPetCard: '#00C3AD',
+  red: '#F43F5E',
 };
 
 export const variables = {
