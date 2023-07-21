7e054a80d9269613944d172f697cd7ccfb2ed5ae
OlgaMunaieva <asyssync@gmail.com>
2023-07-21 16:44:44 +0300

    fixed

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -6,9 +6,11 @@ import { Navigate } from 'react-router-dom';
  * - Otherwise render <Navigate> to redirectTo
  */
 
-export default const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
+const PrivateRoute = ({ component: Component, redirectTo = '/' }) => {
   const { isLoggedIn, isRefreshing } = useAuth();
   const shouldRedirect = !isLoggedIn && !isRefreshing;
 
   return shouldRedirect ? <Navigate to={redirectTo} /> : Component;
 };
+
+export default PrivateRoute;
