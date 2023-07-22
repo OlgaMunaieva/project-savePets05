611714f1eae37e4ab934eaa5e20e17c665f7eca3
OlgaMunaieva <asyssync@gmail.com>
2023-07-22 14:33:22 +0300

    added transition variables

--- src/settings/variables.js
+++ src/settings/variables.js
@@ -2,6 +2,7 @@ const colors = {
   accentHeaderButton: '#FFC107',
   background: '#FDF7F2',
   text: '#000000',
+  secondaryText: '#fef9f9',
   buttonsGr: 'linear-gradient(290.46deg, #419EF1 0%, #9BD0FF 107.89%)',
   buttonsHoverBg: '#54ADFF',
   cardsButtonBg: '#CCE4FB',
@@ -14,9 +15,15 @@ const shadow = {
   shadowHover: '7px 13px 14px 0px #74B1E83D',
 };
 
+const transition = {
+  transitionColor: 'color 250ms cubic-bezier(0.21, 0.65, 0.66, 0.12)',
+  transitionShadow: 'color 250ms cubic-bezier(0.21, 0.65, 0.66, 0.12)',
+};
+
 const variables = {
   colors,
   shadow,
+  transition,
 };
 
 export default variables;
