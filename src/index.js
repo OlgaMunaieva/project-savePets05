3785ffa60f4bc962349e91dd8fb76893eb950b00
OlgaMunaieva <asyssync@gmail.com>
2023-07-22 12:25:39 +0300

    delete StrictMode

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ import { Provider } from 'react-redux';
 import { persistor, store } from 'redux/store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="/project-savePets05">
-          <App />
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter basename="/project-savePets05">
+        <App />
+      </BrowserRouter>
+    </PersistGate>
+  </Provider>
+  // </React.StrictMode>
 );
