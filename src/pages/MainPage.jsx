4840fc3feac23f98820ad37f769a4e8a227d0814
OlgaMunaieva <asyssync@gmail.com>
2023-07-23 11:59:50 +0300

    started with bg img

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,23 +1,28 @@
 import MainContainer from 'components/mainContainer/MainContainer.styled';
+import PictureContainer from 'components/pictureContainer/PictureContainer.styled';
 import variables from 'settings/variables';
 
 export default function MainPage() {
   return (
     <>
       <MainContainer>
-        <div
-          style={{
-            height: '100vh',
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            fontSize: 40,
-            color: `${variables.colors.text}`,
-            border: 2,
-          }}
-        >
-          Take good care of your small pets
-        </div>
+        <PictureContainer.RelativeContainer>
+          <PictureContainer.PawContainer>
+            <div
+              style={{
+                height: '100vh',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                fontSize: 40,
+                color: `${variables.colors.text}`,
+                border: 2,
+              }}
+            >
+              Take good care of your small pets
+            </div>
+          </PictureContainer.PawContainer>
+        </PictureContainer.RelativeContainer>
       </MainContainer>
     </>
   );
