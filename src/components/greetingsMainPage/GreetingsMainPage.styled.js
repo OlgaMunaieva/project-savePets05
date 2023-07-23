8f05fd4cc35ce73b7cdbcba2a6511806bee7188b
OlgaMunaieva <asyssync@gmail.com>
2023-07-24 00:35:38 +0300

    paw

--- src/components/greetingsMainPage/GreetingsMainPage.styled.js
+++ src/components/greetingsMainPage/GreetingsMainPage.styled.js
@@ -2,25 +2,28 @@ import variables from 'settings/variables';
 import styled from 'styled-components';
 
 const GreetingsMainPage = styled.div`
+  padding-left: 20px;
+  padding-top: 108px;
   font-size: 32px;
   font-weight: 700;
   line-height: 44px;
   letter-spacing: 0em;
   text-align: left;
-
-  // width: '100%',
   height: 100vh;
-  // display: 'flex',
-  // justifyContent: 'center',
-  // alignItems: 'center',
-  // fontSize: 40,
   color: ${variables.colors.text};
 
+  ${variables.breakPoints.tablet} {
+    padding-top: 148px;
+    padding-left: 32px;
+    font-size: 68px;
+    line-height: 100px;
+  }
+
   ${variables.breakPoints.desktop} {
-    width: 501px;
-    height: 264px;
     padding-top: 256px;
     padding-left: 16px;
+    width: 501px;
+    min-height: 264px;
     font-size: 68px;
     font-weight: 800;
     line-height: 88px;
