60b9f61e7a1eddd20608c3cedc079cac3c164879
OlgaMunaieva <asyssync@gmail.com>
2023-07-25 00:35:53 +0300

    added notice to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,15 +4,19 @@ import { lazy } from 'react';
 // import PetModal from './petModal/PetModal';
 
 // import AddPetPage from 'pages/AddPetPage';
-import LoginPage from 'pages/LoginPage';
+// import LoginPage from 'pages/LoginPage';
 // import ModalApproveAction from 'pages/ModalApproveAction';
 // import NewsPage from 'pages/NewsPage';
 // import NoticesPage from 'pages/NoticesPage';
 // import OurFriendsPage from 'pages/OurFriendsPage';
-import RegisterPage from 'pages/RegisterPage';
+// import RegisterPage from 'pages/RegisterPage';
+// import NoticesPage from 'pages/NoticesPage';
 // import UserPage from 'pages/UserPage';
 
 const MainPage = lazy(() => import('../pages/MainPage'));
+const RegisterPage = lazy(() => import('../pages/RegisterPage'));
+const LoginPage = lazy(() => import('../pages/LoginPage'));
+const NoticesPage = lazy(() => import('../pages/NoticesPage'));
 
 const App = () => {
   return (
@@ -22,6 +26,7 @@ const App = () => {
         <Route index element={<MainPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/notices" element={<NoticesPage />} />
       </Route>
     </Routes>
     // <div>
