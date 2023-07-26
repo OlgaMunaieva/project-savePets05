36d62677064e8080ebb66120c5e1591ed4b2c4d1
OlgaMunaieva <asyssync@gmail.com>
2023-07-26 14:48:26 +0300

    title on main page

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,16 +1,11 @@
 import GreetingsMainPage from 'components/home/greetingsMainPage/GreetingsMainPage.styled';
 import HomeImages from 'components/home/homeImages/HomeImages';
-import PictureContainer from 'components/pictureContainer/PictureContainer.styled';
 
 export default function MainPage() {
   return (
     <>
-      {/* <PictureContainer.RelativeContainer> */}
-      <PictureContainer.PawContainer>
-        <GreetingsMainPage>Take good care of your small pets</GreetingsMainPage>
-        <HomeImages />
-      </PictureContainer.PawContainer>
-      {/* </PictureContainer.RelativeContainer> */}
+      <GreetingsMainPage>Take good care of your small pets</GreetingsMainPage>
+      <HomeImages />
     </>
   );
 }
