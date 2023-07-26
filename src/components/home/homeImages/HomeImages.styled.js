272ac6a5ccf27a31b7f98b04a19c2cd15b39cf51
OlgaMunaieva <asyssync@gmail.com>
2023-07-26 22:04:58 +0300

    finish with main page picture for mobile

--- src/components/home/homeImages/HomeImages.styled.js
+++ src/components/home/homeImages/HomeImages.styled.js
@@ -6,50 +6,62 @@ const MainPageContainer = styled.div`
 `;
 
 const BigDogContainer = styled.picture`
-  position: absolute;
-  z-index: 3;
+  /* position: absolute; */
+  /* z-index: 3; */
   /* padding-left: 609px; */
   /* max-width: 280px;
   margin-top: -53px; */
 `;
 
 const SmallDogContainer = styled.picture`
-  position: absolute;
-  z-index: 2;
-  top: 140px;
-  left: -40px;
-  width: 205px;
+  /* position: absolute; */
+  /* z-index: 2; */
+  /* top: 140px; */
+  /* left: 0px; */
+  width: 224px;
   /* padding-left: 609px; */
   /* max-width: 280px;
   margin-top: -53px; */
 `;
 
 const CatContainer = styled.picture`
-  position: absolute;
-  z-index: 1;
+  /* position: absolute; */
+  /* z-index: 1; */
   top: 250px;
   left: 104px;
-  width: 174px;
+  width: 186px;
   /* padding-left: 609px; */
   /* max-width: 280px;
   margin-top: -53px; */
 `;
 const BigDogImg = styled.img`
+  /* position: absolute; */
   /* padding-left: 609px; */
   width: 280px;
   margin: -30px 20px;
 `;
 
 const SmallDogImg = styled.img`
+  position: absolute;
+  /* z-index: 2; */
+  top: 150px;
+  left: -20px;
+  width: 224px;
   /* padding-left: 609px; */
-  width: 280px;
-  margin: -30px 20px;
+  /* max-width: 280px;
+  margin-top: -53px; */
+  /* position: absolute; */
+  /* padding-left: 609px; */
+  /* width: 280px; */
+  /* margin: -30px 0px; */
 `;
 
 const CatImg = styled.img`
+  position: absolute;
   /* padding-left: 609px; */
-  width: 280px;
-  margin: -30px 20px;
+  width: 186px;
+  top: 244px;
+  left: 112px;
 `;
 
 const Pictures = {
