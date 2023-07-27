deffb5a098481a27cb4179fc308c3367e0703336
Демченко Сергей <Demniks@ukr.net>
2023-07-27 17:49:58 +0300

    create friendlist

---
+++ src/components/OurFriends/OurFriendsList/OurFriendsList.styled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+import variables from 'settings/variables';
+
+export const FriendsList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 20px;
+  margin-top: 24px;
+
+  ${variables.breakPoints.tablet} {
+    flex-direction: row;
+    flex-wrap: wrap;
+    gap: 32px;
+  }
+`;
