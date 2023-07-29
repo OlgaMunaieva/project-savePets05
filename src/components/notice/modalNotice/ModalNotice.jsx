55c89f1b708bd963a73f0eb846321ec3580d7098
OlgaMunaieva <asyssync@gmail.com>
2023-07-29 14:15:04 +0300

    something

--- src/components/notice/modalNotice/ModalNotice.jsx
+++ src/components/notice/modalNotice/ModalNotice.jsx
@@ -1,24 +1,57 @@
-import { useNavigate } from 'react-router-dom';
-import Modal from '../../components/modal/Modal';
-import {
-  StyledContainer,
-  StyledTitle,
-  StyledText,
-  StyledButton,
-} from './ModalCongrats.styled';
+import { useCallback, useEffect } from 'react';
+import { createPortal } from 'react-dom';
 
-const ModalNotice = ({ closeModal, openModal }) => {
-  const navigate = useNavigate();
-  return (
-    <Modal closeModal={closeModal} isOpenedModal={openModal} width="610px">
-      <StyledContainer>
-        <StyledTitle>Congrats</StyledTitle>
-        <StyledText>You're registration is success</StyledText>
-        <StyledButton onClick={() => navigate('/user')}>
-          Go to profile
-        </StyledButton>
-      </StyledContainer>
-    </Modal>
+import {
+  ModalWindow,
+  ModalContent,
+  ExitButton,
+} from '../../modal/Modal.styled';
+
+import CrossIcon from '../../../images/icons/CrossIcon.svg';
+
+const modalRoot = document.body;
+
+const ModalNotice = ({ closeModal, isOpenedModal, children }) => {
+  const toggleModal = useCallback(
+    e => {
+      if (e.code !== 'Escape') return;
+      closeModal();
+    },
+    [closeModal]
+  );
+
+  useEffect(() => {
+    window.addEventListener('keydown', toggleModal);
+
+    return () => {
+      window.removeEventListener('keydown', toggleModal);
+    };
+  }, [isOpenedModal, toggleModal]);
+
+  useEffect(() => {
+    const handleKeyDown = e => toggleModal(e);
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [toggleModal]);
+
+  const onClickOverlay = e => {
+    if (e.target === e.currentTarget) closeModal();
+  };
+
+  return createPortal(
+    <ModalWindow onClick={onClickOverlay}>
+      <ModalContent>
+        <ExitButton onClick={() => closeModal()}>
+          <img src={CrossIcon} alt="Cross" width={24} height={24} />
+        </ExitButton>
+        {children}
+      </ModalContent>
+    </ModalWindow>,
+    modalRoot
   );
 };
 
---
+++ src/components/notice/modalNotice/ModalNotice.styled.js
@@ -0,0 +1,6 @@
+import { styled } from 'styled-components';
+import Modal from '../../modal/Modal';
+
+export const ModalNoticeContainer = styled(Modal)`
+  width: 1000px;
+`;
