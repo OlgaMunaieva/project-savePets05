1c2bbdf91a1b5e67e5c59a95bd8c8bd94bb1bfad
OlgaMunaieva <asyssync@gmail.com>
2023-07-29 23:55:06 +0300

    some changes

--- src/components/SharedLayout.jsx
+++ src/components/SharedLayout.jsx
@@ -7,13 +7,16 @@ import PictureContainer from './pictureContainer/PictureContainer.styled';
 
 const SharedLayout = () => {
   const location = useLocation();
+  console.log(location.pathname);
   const ContainerToRender =
     location.pathname === '/notices/sell' ||
     location.pathname === '/notices' ||
     location.pathname === '/news' ||
     location.pathname === '/friends' ||
     location.pathname === '/notices/lost-found' ||
-    location.pathname === ' /notices/for-free'
+    location.pathname === '/notices/favorite' ||
+    location.pathname === '/notices/own' ||
+    location.pathname === '/notices/for-free'
       ? MainContainer
       : PictureContainer.PawContainer;
 
