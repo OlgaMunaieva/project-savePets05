772e55071efff98e61724538759b52e3d8d56f5f
Демченко Сергей <Demniks@ukr.net>
2023-07-30 15:13:07 +0300

    update friendList

--- src/components/OurFriends/OurFriendsList/OurFriendsList.jsx
+++ src/components/OurFriends/OurFriendsList/OurFriendsList.jsx
@@ -1,35 +1,22 @@
-import axios from 'axios';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import OurFriendsItem from '../../OurFriends/OurFriendsItem/OurFriendsItem';
 import { FriendsList } from '../OurFriendsList/OurFriendsList.styled';
 
-const baseURL = 'https://64bfe06f0d8e251fd1119ee8.mockapi.io/friends';
+export const OurFriendsList = ({ items }) => {
+  const [hoveredTime, setHoveredTime] = useState('');
 
-export const getOurFriends = async () => {
-  try {
-    const response = await axios.get(baseURL);
-    const friends = response.data;
-    // console.log(friends);
-    return friends;
-  } catch (error) {
-    console.error(error);
-  }
-};
+  const handleTimeHover = index => {
+    setHoveredTime(index);
+  };
 
-export const OurFriendsList = ({ friends }) => {
   return (
     <FriendsList>
-      {friends.map(({ id, name, avatar, time, adress, phone, email }) => (
-        <OurFriendsItem
-          key={id}
-          name={name}
-          avatar={avatar}
-          time={time}
-          adress={adress}
-          phone={phone}
-          email={email}
-        />
-      ))}
+      <OurFriendsItem
+        items={items}
+        handleTimeHover={handleTimeHover}
+        hoveredTime={hoveredTime}
+      />
     </FriendsList>
   );
 };
@@ -37,9 +24,5 @@ export const OurFriendsList = ({ friends }) => {
 // export default OurFriendsList;
 
 OurFriendsList.propTypes = {
-  friends: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.string.isRequired,
-    })
-  ),
+  items: PropTypes.array.isRequired,
 };
