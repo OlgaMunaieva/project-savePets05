290a7fd6b5499b9b35926016f545ee0ac71d37de
OlgaMunaieva <asyssync@gmail.com>
2023-07-30 18:43:28 +0300

    доработанная модалка

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -1,27 +1,46 @@
-import { useEffect, useRef } from 'react';
+import { useCallback, useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 import { ModalWindow, ModalContent, ExitButton } from './Modal.styled';
 
-import CrossIcon from '..//../images/icons/CrossIcon.svg';
+import CrossIcon from '../../images/icons/CrossIcon.svg';
 
 const modalRoot = document.body;
 
-const Modal = ({ closeModal, isOpenedModal, children, width }) => {
-  const modalContentRef = useRef(null);
+const Modal = ({ closeModal, isOpenedModal, children, width, padding }) => {
+  const toggleModal = useCallback(
+    e => {
+      if (e.code !== 'Escape') return;
+      closeModal();
+    },
+    [closeModal]
+  );
 
   useEffect(() => {
-    window.addEventListener('keydown', toggleModal);
+    if (isOpenedModal) {
+      const scrollY = window.scrollY;
+      document.body.style.position = 'fixed';
+      document.body.style.top = `-${scrollY}px`;
+    }
 
     return () => {
       window.removeEventListener('keydown', toggleModal);
+      const scrollY = parseInt(document.body.style.top || '0', 10);
+      document.body.style.position = '';
+      document.body.style.top = '';
+      window.scrollTo(0, -scrollY);
     };
-  }, [isOpenedModal]);
+  }, [isOpenedModal, toggleModal]);
 
-  const toggleModal = e => {
-    if (e.code !== 'Escape') return;
-    closeModal();
-  };
+  useEffect(() => {
+    const handleKeyDown = e => toggleModal(e);
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [toggleModal]);
 
   const onClickOverlay = e => {
     if (e.target === e.currentTarget) closeModal();
@@ -29,7 +48,7 @@ const Modal = ({ closeModal, isOpenedModal, children, width }) => {
 
   return createPortal(
     <ModalWindow onClick={onClickOverlay}>
-      <ModalContent style={{ width: width }} ref={modalContentRef}>
+      <ModalContent style={{ width: width, padding: padding }}>
         <ExitButton onClick={() => closeModal()}>
           <img src={CrossIcon} alt="Cross" width={24} height={24} />
         </ExitButton>
