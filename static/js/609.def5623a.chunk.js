2737989372fdbc994661d1dde5b4f7694a0a94db
OlgaMunaieva <111134698+OlgaMunaieva@users.noreply.github.com>
2023-07-31 15:23:47 +0000

    Deploying to gh-pages from @ OlgaMunaieva/project-savePets05@db06d9f893d99a33692bc2fbecd76be270acc1bd 🚀

--- static/js/609.def5623a.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkproject_savePets05=self.webpackChunkproject_savePets05||[]).push([[609],{7609:function(e,s,t){t(2791);s.Z=t.p+"static/media/sprite.a6788f75cf092c6527e25913d86ef316.svg"}}]);
-//# sourceMappingURL=609.def5623a.chunk.js.map
