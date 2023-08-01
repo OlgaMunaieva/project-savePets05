906d785469fc749eabb18e726e8c55a63398014a
OlgaMunaieva <asyssync@gmail.com>
2023-08-01 17:49:26 +0300

    user

---
+++ src/components/user/userTitle/UserTitle.jsx
@@ -0,0 +1,3 @@
+export const UserTitle = (children) => {
+  return <UserTitle>{children}</UserTitle>
+}
---
+++ src/components/user/userTitle/UserTitle.styled.js
@@ -0,0 +1,11 @@
+import variables from "settings/variables";
+import { styled } from "styled-components";
+
+export const UserTitle = styled.h2`
+  font-size: 28px;
+  font-weight: 500;
+  color: ${variables.colors.simpleWhite};
+  line-height: 1.36;
+  letter-spacing: 0em;
+  margin-bottom: 24px;
+`;
