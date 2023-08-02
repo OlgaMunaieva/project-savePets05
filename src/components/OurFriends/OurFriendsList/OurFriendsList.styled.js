cee443a82a9a9b17ea59f7730b2e65d36f8615c9
Демченко Сергей <Demniks@ukr.net>
2023-08-03 01:26:07 +0300

    fix fonts

--- src/components/OurFriends/OurFriendsList/OurFriendsList.styled.js
+++ src/components/OurFriends/OurFriendsList/OurFriendsList.styled.js
@@ -8,6 +8,7 @@ export const FriendsList = styled.ul`
   align-items: center;
   gap: 20px;
   margin-top: 24px;
+  margin-bottom:70px;
 
   ${variables.breakPoints.tablet} {
     flex-direction: row;
